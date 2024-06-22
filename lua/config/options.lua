a6c36b8a7e5bd8cfa26d3c0dca36342a4b1b018b
Jonathan Shieh <jonathan.shieh@gmail.com>
2024-06-22 18:04:07 +0800

    Moved some keybinds over

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,6 +5,9 @@
 local cmd = vim.cmd -- execute Vim commands
 local opt = vim.opt
 local g = vim.g
+local map = vim.keymap.set
+
+g.maplocalleader = "," -- Local leader is comma
 
 -- Reverse ; and :
 -- This makes it easier to access commands as I rarely repeat commands
@@ -14,3 +17,14 @@ cmd([[
 	vnoremap ; :
 	vnoremap : ;
 ]])
+
+-- Keep the cursor in the middle of the screen after paging up/down
+map("n", "<C-d>", "<C-d>zz")
+map("n", "<C-y>", "<C-u>zz")
+
+-- Move blocks of code up and down using J and K in visual mode
+map("v", "J", ":m '>+1<CR>gv=gv")
+map("v", "K", ":m '>-3<CR>gv=gv")
+
+-- Enable pasting over current selection
+map("x", "<leader>p", '"_dP')
