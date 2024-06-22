52c46d5166284bfa52ea90bb82125ddbf4e3ed84
Jonathan Shieh <jonathan.shieh@gmail.com>
2024-06-22 14:59:57 +0800

    <C-P> brings up the project files search

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+local wk = require("which-key")
+
+wk.register({
+  ["<C-p>"] = { LazyVim.pick("auto"), "Find File" },
+}, { prefix = "" })
