5bf798ed81bf0ef6c807e2b4eaf49c585543bda9
Jonathan Shieh <jonathan.shieh@gmail.com>
2024-06-22 15:00:30 +0800

    Reverse ";" and ":"

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,16 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+local cmd = vim.cmd -- execute Vim commands
+local opt = vim.opt
+local g = vim.g
+
+-- Reverse ; and :
+-- This makes it easier to access commands as I rarely repeat commands
+cmd([[
+	nnoremap ; :
+	nnoremap : ;
+	vnoremap ; :
+	vnoremap : ;
+]])
